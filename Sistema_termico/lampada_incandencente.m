5ea32b36243324b88ec44966bbcf4444eb878070
leticia <leticiarp2000@hotmail.com>
2023-10-08 01:06:03 -0300

    add plots para variaveis do PID

--- Sistema_termico/lampada_incandencente.m
+++ Sistema_termico/lampada_incandencente.m
@@ -2,99 +2,125 @@
 clc;
 clear all;
 
-% dicas:
-% 1 - colocar ; no final do comando faz com que o output NAO apareça no console
-% 2 - comandos SEM ; no final, faz com que APAREÇA o output no console
+% ARDUINO ----------------------------------------------------------------------------------------------------------------------------
+%a = arduino('COM3', 'Uno', 'Libraries', 'Adafruit/DHTxx');
 
-% criar objeto 'arduino' no matlab e inicia a comunicação serial com o software
-ino = arduino('COM7', 'Uno', 'Libraries', 'Adafruit/DHTxx')
+% SENSOR ----------------------------------------------------------------------------------------------------------------------------
+%sensor_dht = addon(a, 'Adafruit/DHTxx', 'D5','DHT11');
 
-% criar objeto sensor
-sensor_dht = addon(a, 'Adafruit/DHTxx', 'D3','DHT11')
-
+% CONSTANTES PID ---------------------------------------------------------------------------------------------------------------------
 SETPOINT = 50;
-
-KP = 1;
-KI = 0;
+KP = 2.5;
+KI = 0.1;
 KD = 0;
-low = 0;
+
+% Define os limites do PID
+minPID = 10;
+maxPID = -10;
+
+% VARIAVEIS PID ----------------------------------------------------------------------------------------------------------------------
 pid = 0;
 last_pid = 0;
 
-error = 0;
-last_error = 0;
-last2_error = 0;
+% measure = readTemperature(sensor_dht);
+measure = 25; 
+error = measure - SETPOINT; 
+last_error = error; 
+last2_error = error; 
 
-measure = 0;
-last_sample = 25;
 
+% PWM ---------------------------------------------------------------------------------------------------------------------------------
+low = 0;
+high = 0;
 duty = 0;
+minDuty = 0;
+maxDuty = 1;
+signal_frequency = 2; 
 
-ts = 0.5;
+% TEMPO DE AMOSTRAGEM ------------------------------------------------------------------------------------------------------------------
+ts = 2;
 current_time = tic;
 elapse_time = toc(current_time);
 
-% Configuração do gráfico em tempo real
-% figure;
-% ax = axes;
-% line = animatedline(ax, 'Color', 'b'); % Configura a cor da linha
 
-% xlabel('Tempo');
-% ylabel('Medida');
-% title('Leitura em Tempo Real');
+% GRAFICO ------------------------------------------------------------------------------------------------------------------------------ 
+figure(1); 
+xlabel('Tempo');
+ylabel("Temperatura");
+title("Controlador de Temperatura");
+temperature = animatedline('Color', 'b', 'LineWidth', 1.5); 
+setpoint = animatedline('Color', 'r', 'LineStyle', '--', 'LineWidth', 1.5);
+setpoint.DisplayName = 'setpoint';
+temperature.DisplayName = 'temperatura'
+ylim([20, 70]); 
+legend('show');
 
-% Defina os limites dos eixos, se necessário
-% ylim([ymin, ymax]);
+figure(2); 
+subplot(2,3,[1,2,3]); 
+xlabel('Tempo'); 
+ylabel('Temperatura'); 
+title('Output PID'); 
+pid_output = animatedline('Color', 'm', 'LineWidth', 1.5);
 
-writeDigitalPin(ino, 'D13', 0)
+subplot(2,3,4); 
+title('Proporcional'); 
+p_output = animatedline('Color', 'g', 'LineWidth', 1.5);
+
+subplot(2,3,5); 
+title('Integrativo'); 
+i_output = animatedline('Color', 'g', 'LineWidth', 1.5);
+
+subplot(2,3,6); 
+title('Derivativo'); 
+d_output = animatedline('Color', 'g', 'LineWidth', 1.5);
+
 
 while (true)
-    measure = readTemperature(sensor_dht);
-    
-    if (last_sample - measure) > 5 
-        measure = last_sample; 
-    end 
+    %measure = readTemperature(sensor_dht);
+    measure = 25;
     
     elapse_time = toc(current_time);
+
     if (elapse_time > ts)
         
-        error = measure - SETPOINT
+        error = measure - SETPOINT;
 
-        pid = last_pid + error * (KP + KI * ts + KD / ts) - last_error * (KP + (2 * KD) / 2) + last2_error * (KD / ts);
+        pid = last_pid + error * (KP + KI * ts + KD / ts) - last_error * (KP + (2 * KD) / ts) + last2_error * (KD / ts);
 
-        duty = abs(1 - ((pid - (-20)) * (0 - 1) / (35 - (-20)) + 1));
-        
-        if duty > 1
-            duty = 1
-        end 
-        
-        if duty < 0
-            duty = 0
-        end
-        
-        measure = measure
-        
-        % % Atualize o gráfico em tempo real
-        % addpoints(line, toc(current_time), measure);
-        % drawnow limitrate; % Limita a taxa de atualização para manter um gráfico mais suave
+        proporcional = KP*(error - last_error); 
+        integrativo = KI*(error * ts); 
+        derivativo = KD*(error - 2*last_error + last2_error)/ts;
 
-        % COLOCAR O PWM MANUALMENTE
+        duty = (pid - minPID) / (maxPID - minPID) * (maxDuty - minDuty) + minDuty;
+        duty = max(minDuty, min(maxDuty, duty));
+
+        time_axis = round(double(current_time), 2)
+        
+        addpoints(temperature, time_axis, measure);
+        addpoints(setpoint, time_axis, SETPOINT);
+        addpoints(pid_output, time_axis, pid); 
+        addpoints(p_output, time_axis, proporcional); 
+        addpoints(i_output, time_axis, integrativo); 
+        addpoints(d_output, time_axis, derivativo); 
+
+        drawnow;
+
         current_time = tic;
+        % fprintf('Temp=%d | Ts=%d | Error=%d PID=%d|Duty=%d\n', measure,elapse_time,error,pid,duty);
+        fprintf('PID = %d | P = %d | I = %d | D = %d \n', pid, proporcional, integrativo, derivativo);
     end
 
-    generatePWM(ino, 0.5, duty);
+    generatePWM(1, signal_frequency, duty);
 
     last_pid = pid;
     last_error = error;
     last2_error = last_error;
-    last_sample = measure
-
 end
 
 function generatePWM(ino, frequency, dutyCycle)
     % Parâmetros:
     % - frequency: Frequência do sinal PWM em Hertz
-    % - dutyCycle: Ciclo de trabalho em porcentagem (0 a 100)
+    % - dutyCycle: Ciclo de trabalho em porcentagem (0 a 10)
 
     % Verifica se o ciclo de trabalho está dentro do intervalo permitido
     if dutyCycle < 0 || dutyCycle > 1
@@ -105,15 +131,18 @@ function generatePWM(ino, frequency, dutyCycle)
     period = 1 / frequency;
 
     % Calcula o tempo ativo (ligado) com base no ciclo de trabalho
-    activeTime = (dutyCycle / 1) * period;
+    high = (dutyCycle / 1) * period;
 
-    % Liga o sina
-    writeDigitalPin(ino, 'D13', 1);
-    pause(activeTime);
+    % Liga o sinal
+    if high ~= 0
+    %    writeDigitalPin(ino, 'D7', 1);
+       pause(high);
+    end
 
     % Desliga o sinal
-    writeDigitalPin(ino, 'D13', 0);
-    low = period - activeTime;
-    % Espera por um novo ciclo
-    pause(low);
+    low = period - high;
+    if low ~= 0
+        % writeDigitalPin(ino, 'D7', 0);
+        pause(low);
+    end
 end
